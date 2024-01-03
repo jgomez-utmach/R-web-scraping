126dcee47766060b1cb8d65837d5bb051387e236
jgomez-utmach <jgomez20@utmachala.edu.ec>
2024-01-03 01:21:01 -0500

    URLs de paginación

--- scraping.r
+++ scraping.r
@@ -1,16 +1,30 @@
 library(rvest)
-# URL de la página con la lista de productos
+
 url <- "https://www.amazon.com/s?k=aspiradora&crid=HU5HMDQLN0QT&sprefix=aspira%2Caps%2C357&ref=nb_sb_ss_ts-doa-p_3_6"
-# Selector de un producto en la lista
+
 selector <- "#search > div.s-desktop-width-max.s-desktop-content.s-wide-grid-style-t1.s-opposite-dir.s-wide-grid-style.sg-row > div.sg-col-20-of-24.s-matching-dir.sg-col-16-of-20.sg-col.sg-col-8-of-12.sg-col-12-of-16 > div > span.rush-component.s-latency-cf-section > div.s-main-slot.s-result-list.s-search-results.sg-row > div:nth-child(9) > div > div > span > div > div > div > div.puisg-col.puisg-col-4-of-12.puisg-col-8-of-16.puisg-col-12-of-20.puisg-col-12-of-24.puis-list-col-right > div > div > div.a-section.a-spacing-none.puis-padding-right-small.s-title-instructions-style > h2 > a"
 
-# Leemos la página
 pagina <- read_html(url)
-# Extraemos el nodo que coincide con el selector
 nodo <- html_nodes(pagina, selector)
-# Extraemos el link del nodo
 nodo_links <- html_attr(nodo, "href")
-# Unimos el dominio con el link
-# paste0: Une Strings sin espacios
 urlcompleta <- paste0("https://www.amazon.com", nodo_links)
-urlcompleta
+
+
+# ANALIZAMOS ESTRUCTURA DE LOS LINKS DE PAGINACIÓN
+# ( Nos fijamos que la url de paginación cambia aumentando page=# y ref=sr_pg_# )
+# https://www.amazon.com/-/es/s?k=aspiradora&page=2&crid=HU5HMDQLN0QT&qid=1704235659&sprefix=aspira%2Caps%2C357&ref=sr_pg_2
+# https://www.amazon.com/-/es/s?k=aspiradora&page=3&crid=HU5HMDQLN0QT&qid=1704235659&sprefix=aspira%2Caps%2C357&ref=sr_pg_3
+
+# Importamos librería que nos perminte manipular strings
+library(stringr)
+
+pag <- "https://www.amazon.com/-/es/s?k=aspiradora&page=2&crid=HU5HMDQLN0QT&qid=1704235659&sprefix=aspira%2Caps%2C357&ref=sr_pg_2"
+
+# Crea vector con 10 números del 1 al 10
+lista_paginas <- c(1:10)
+
+# Modica la url de pag para obtener un vector con 10 ulrs de paginación
+# str_replace: Reemplaza caracteres de un string por otro
+pag <- str_replace(pag, "page=2", paste0("page=", lista_paginas))
+pag <- str_replace(pag, "sr_pg_2", paste0("sr_pg_", lista_paginas))
+pag
