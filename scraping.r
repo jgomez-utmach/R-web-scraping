e3e0639945e506e79cd090659fde803facc47f55
jgomez-utmach <jgomez20@utmachala.edu.ec>
2024-01-03 01:23:16 -0500

    Extrae links de cada página

--- scraping.r
+++ scraping.r
@@ -10,21 +10,37 @@ nodo_links <- html_attr(nodo, "href")
 urlcompleta <- paste0("https://www.amazon.com", nodo_links)
 
 
-# ANALIZAMOS ESTRUCTURA DE LOS LINKS DE PAGINACIÓN
-# ( Nos fijamos que la url de paginación cambia aumentando page=# y ref=sr_pg_# )
-# https://www.amazon.com/-/es/s?k=aspiradora&page=2&crid=HU5HMDQLN0QT&qid=1704235659&sprefix=aspira%2Caps%2C357&ref=sr_pg_2
-# https://www.amazon.com/-/es/s?k=aspiradora&page=3&crid=HU5HMDQLN0QT&qid=1704235659&sprefix=aspira%2Caps%2C357&ref=sr_pg_3
-
-# Importamos librería que nos perminte manipular strings
 library(stringr)
 
 pag <- "https://www.amazon.com/-/es/s?k=aspiradora&page=2&crid=HU5HMDQLN0QT&qid=1704235659&sprefix=aspira%2Caps%2C357&ref=sr_pg_2"
 
-# Crea vector con 10 números del 1 al 10
 lista_paginas <- c(1:10)
 
-# Modica la url de pag para obtener un vector con 10 ulrs de paginación
-# str_replace: Reemplaza caracteres de un string por otro
 pag <- str_replace(pag, "page=2", paste0("page=", lista_paginas))
 pag <- str_replace(pag, "sr_pg_2", paste0("sr_pg_", lista_paginas))
-pag
+
+
+# Creamos una funcion que extrae los links de una pagina
+dameLinksPagina <- function(url){
+  selector <- "div > div > span > div > div > div > div.puisg-col.puisg-col-4-of-12.puisg-col-8-of-16.puisg-col-12-of-20.puisg-col-12-of-24.puis-list-col-right > div > div > div.a-section.a-spacing-none.puis-padding-right-small.s-title-instructions-style > h2 > a"
+  pagina <- read_html(url)
+  nodo <- html_nodes(pagina, selector)
+  nodo_links <- html_attr(nodo, "href")
+  urlcompleta <- paste0("https://www.amazon.com", nodo_links)
+  urlcompleta
+}
+
+# Testeamos la funcion
+test <- dameLinksPagina(pag[1])
+
+# sapply aplica una funcion (dameLinksPagina) a cada elemento de un vector (pag)
+linksAsp <- sapply(pag, dameLinksPagina)
+
+# NOTA: sapply devuelve una matriz, en mi caso de 22 filas y 10 columnas
+dim(linksAsp)
+
+# Por comodidad lo convertimos en un vector
+vlink <- as.vector(linksAsp)
+
+# Vemos cuantos elementos tiene el vector
+length(vlink)
