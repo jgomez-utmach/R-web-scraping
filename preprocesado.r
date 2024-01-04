206d8b88d6e100b132a1acbfb363dd833a259a84
jgomez-utmach <jgomez20@utmachala.edu.ec>
2024-01-04 16:00:50 -0500

    Eliminando filas con datos invalidos

--- preprocesado.r
+++ preprocesado.r
@@ -1,35 +1,55 @@
-# Modificamos archivos CSV debido a que no se pudo obtener datos sobre el peso
-# y añadimos los datos manualmente en datos-1.csv
-# Esto es con el fin de ver como se hace un tratamiento de datos
 
-# Leer un archivo CSV
 aspiradoras <- read.csv("datos-1.csv")
 
 
-# PROCESADO PARA EL PESO
-
-# Remplazar "kg" por "" en la columna Peso
 aspiradoras$Peso <- gsub(" kg", "", aspiradoras$Peso)
-# Remplazar "," por "." en la columna Peso
 aspiradoras$Peso <- gsub(",", ".", aspiradoras$Peso)
 
-# Identificar las filas donde Peso está en gramos
 es_gramo <- grepl(" g", aspiradoras$Peso)
-# Remplazar " g" por "" en la columna Peso
 aspiradoras$Peso <- gsub(" g", "", aspiradoras$Peso)
 
-# Remplazar "-1" por NA en la columna Peso
 aspiradoras$Peso <- gsub("-1", NA, aspiradoras$Peso)
 
-# Convertir la columna Peso a numérico
 aspiradoras$Peso <- as.numeric(aspiradoras$Peso)
 
-# Convertir solo los valores en gramos a kilogramos
 aspiradoras$Peso[es_gramo] <- aspiradoras$Peso[es_gramo] / 1000
 
-# Calcular el peso medio de los productos con mean()
-# na.rm = TRUE para que no tome en cuenta los NA
 pesomedio <- mean(aspiradoras$Peso, na.rm = TRUE)
 
-# Remplazar los NA por el peso medio
 aspiradoras$Peso[is.na(aspiradoras$Peso)] <- pesomedio
+
+
+# ELIMINANDO FILAS CON DATOS INVALIDOS
+
+# Crear una copia de aspiradoras sin la columna X
+aspiradoras_sin_X <- aspiradoras[, !(names(aspiradoras) %in% "X")]
+
+# Identificar las filas donde todos los campos son NA o -1
+# apply(X, MARGIN, FUN): Aplica una función a los márgenes de una matriz o un data frame
+# X=matriz o data frame, MARGIN=1 para filas, 2 para columnas, FUN=función a aplicar
+# all(): Determina si todos los valores lógicos de cada fila son TRUE
+filas_invalidas <- apply(aspiradoras_sin_X == -1 | is.na(aspiradoras_sin_X), 1, all)
+
+# Eliminar las filas invalidas
+aspiradoras <- aspiradoras[!filas_invalidas, ]
+
+
+# EN CASO DE TENER DATOS SIN NOMBRE
+
+# Eliminar las filas con NA en la columna Nombre
+aspiradoras <- aspiradoras[!is.na(aspiradoras$Nombre), ]
+
+# Obtener la cantidad de filas
+n_filas <- nrow(aspiradoras)
+
+# Crear una columna X con los números de las filas
+aspiradoras$X <- c(1:n_filas)
+
+# Asignar los valores de la columna X como nombres de las filas
+rownames(aspiradoras) <- aspiradoras$X
+
+# Crear un histograma de los pesos
+hist(aspiradoras$Peso, main = "Histograma de pesos", xlab = "Peso (kg)")
+
+# Ver los datos
+View(aspiradoras)
