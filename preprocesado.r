2eb9c2fc4f14b70620b6835debe390fd25841df1
jgomez-utmach <jgomez20@utmachala.edu.ec>
2024-01-04 20:23:06 -0500

    Preprocesado de Dimensiones

--- preprocesado.r
+++ preprocesado.r
@@ -4,52 +4,70 @@ aspiradoras <- read.csv("datos-1.csv")
 
 aspiradoras$Peso <- gsub(" kg", "", aspiradoras$Peso)
 aspiradoras$Peso <- gsub(",", ".", aspiradoras$Peso)
-
 es_gramo <- grepl(" g", aspiradoras$Peso)
 aspiradoras$Peso <- gsub(" g", "", aspiradoras$Peso)
-
 aspiradoras$Peso <- gsub("-1", NA, aspiradoras$Peso)
-
 aspiradoras$Peso <- as.numeric(aspiradoras$Peso)
-
 aspiradoras$Peso[es_gramo] <- aspiradoras$Peso[es_gramo] / 1000
-
 pesomedio <- mean(aspiradoras$Peso, na.rm = TRUE)
-
 aspiradoras$Peso[is.na(aspiradoras$Peso)] <- pesomedio
 
-
-# ELIMINANDO FILAS CON DATOS INVALIDOS
-
-# Crear una copia de aspiradoras sin la columna X
 aspiradoras_sin_X <- aspiradoras[, !(names(aspiradoras) %in% "X")]
-
-# Identificar las filas donde todos los campos son NA o -1
-# apply(X, MARGIN, FUN): Aplica una función a los márgenes de una matriz o un data frame
-# X=matriz o data frame, MARGIN=1 para filas, 2 para columnas, FUN=función a aplicar
-# all(): Determina si todos los valores lógicos de cada fila son TRUE
 filas_invalidas <- apply(aspiradoras_sin_X == -1 | is.na(aspiradoras_sin_X), 1, all)
-
-# Eliminar las filas invalidas
 aspiradoras <- aspiradoras[!filas_invalidas, ]
 
-
-# EN CASO DE TENER DATOS SIN NOMBRE
-
-# Eliminar las filas con NA en la columna Nombre
 aspiradoras <- aspiradoras[!is.na(aspiradoras$Nombre), ]
-
-# Obtener la cantidad de filas
 n_filas <- nrow(aspiradoras)
-
-# Crear una columna X con los números de las filas
 aspiradoras$X <- c(1:n_filas)
-
-# Asignar los valores de la columna X como nombres de las filas
 rownames(aspiradoras) <- aspiradoras$X
 
-# Crear un histograma de los pesos
-hist(aspiradoras$Peso, main = "Histograma de pesos", xlab = "Peso (kg)")
 
-# Ver los datos
-View(aspiradoras)
+# PREPROCESADO DE DIMENSIONES
+
+# Limpiar datos de dimensiones
+aspiradoras$Dimensiones <- gsub("-1", "-1 x -1 x -1", aspiradoras$Dimensiones)
+aspiradoras$Dimensiones <- gsub(" pulgadas", "", aspiradoras$Dimensiones)
+aspiradoras$Dimensiones <- gsub("\"l.", "", aspiradoras$Dimensiones)
+aspiradoras$Dimensiones <- gsub("\"an.", "", aspiradoras$Dimensiones)
+aspiradoras$Dimensiones <- gsub("\"al.", "", aspiradoras$Dimensiones)
+aspiradoras$Dimensiones <- gsub(",", ".", aspiradoras$Dimensiones)
+
+# Importar libreria para manipular strings
+library(stringr)
+
+# Separar las dimensiones en 3 columnas a partir del caracter x
+dimen <-  str_split_fixed(aspiradoras$Dimensiones, "x", 3)
+
+# Convertir dimen en un data.frame
+dimen <- as.data.frame(dimen)
+
+# Cambiar el nombre de las columnas
+colnames(dimen) <- c("Largo", "Ancho", "Alto")
+
+# Reemplazar los -1 por NA
+dimen$Largo <- gsub("-1", NA, dimen$Largo)
+dimen$Ancho <- gsub("-1", NA, dimen$Ancho)
+dimen$Alto <- gsub("-1", NA, dimen$Alto)
+
+# Convertir datos de dimen a numéricos
+dimen$Largo <- as.numeric(dimen$Largo)
+dimen$Ancho <- as.numeric(dimen$Ancho)
+dimen$Alto <- as.numeric(dimen$Alto)
+
+# Convertir todos los datos de dimen de pulgadas a centimetros
+dimen$Largo <- as.numeric(dimen$Largo) * 2.54
+dimen$Ancho <- as.numeric(dimen$Ancho) * 2.54
+dimen$Alto <- as.numeric(dimen$Alto) * 2.54
+
+# Reemplazar los NA por la media de cada columna
+dimen$Largo[is.na(dimen$Largo)] <- mean(dimen$Largo, na.rm = TRUE)
+dimen$Ancho[is.na(dimen$Ancho)] <- mean(dimen$Ancho, na.rm = TRUE)
+dimen$Alto[is.na(dimen$Alto)] <- mean(dimen$Alto, na.rm = TRUE)
+
+# cbind para unir columnas de dos data.frames
+res_limpio <- cbind(aspiradoras, dimen)
+
+# Quitamos la columna Dimensiones
+res_limpio <- res_limpio[, -7]
+
+View(res_limpio)
