24dfb6e0d80864cbb4438df44017a4de265cb3df
jgomez-utmach <jgomez20@utmachala.edu.ec>
2024-01-04 21:46:54 -0500

    Preprocesados de Precios

--- preprocesado.r
+++ preprocesado.r
@@ -22,9 +22,6 @@ aspiradoras$X <- c(1:n_filas)
 rownames(aspiradoras) <- aspiradoras$X
 
 
-# PREPROCESADO DE DIMENSIONES
-
-# Limpiar datos de dimensiones
 aspiradoras$Dimensiones <- gsub("-1", "-1 x -1 x -1", aspiradoras$Dimensiones)
 aspiradoras$Dimensiones <- gsub(" pulgadas", "", aspiradoras$Dimensiones)
 aspiradoras$Dimensiones <- gsub("\"l.", "", aspiradoras$Dimensiones)
@@ -32,42 +29,48 @@ aspiradoras$Dimensiones <- gsub("\"an.", "", aspiradoras$Dimensiones)
 aspiradoras$Dimensiones <- gsub("\"al.", "", aspiradoras$Dimensiones)
 aspiradoras$Dimensiones <- gsub(",", ".", aspiradoras$Dimensiones)
 
-# Importar libreria para manipular strings
 library(stringr)
-
-# Separar las dimensiones en 3 columnas a partir del caracter x
 dimen <-  str_split_fixed(aspiradoras$Dimensiones, "x", 3)
-
-# Convertir dimen en un data.frame
 dimen <- as.data.frame(dimen)
 
-# Cambiar el nombre de las columnas
 colnames(dimen) <- c("Largo", "Ancho", "Alto")
 
-# Reemplazar los -1 por NA
 dimen$Largo <- gsub("-1", NA, dimen$Largo)
 dimen$Ancho <- gsub("-1", NA, dimen$Ancho)
 dimen$Alto <- gsub("-1", NA, dimen$Alto)
 
-# Convertir datos de dimen a numéricos
 dimen$Largo <- as.numeric(dimen$Largo)
 dimen$Ancho <- as.numeric(dimen$Ancho)
 dimen$Alto <- as.numeric(dimen$Alto)
 
-# Convertir todos los datos de dimen de pulgadas a centimetros
 dimen$Largo <- as.numeric(dimen$Largo) * 2.54
 dimen$Ancho <- as.numeric(dimen$Ancho) * 2.54
 dimen$Alto <- as.numeric(dimen$Alto) * 2.54
 
-# Reemplazar los NA por la media de cada columna
 dimen$Largo[is.na(dimen$Largo)] <- mean(dimen$Largo, na.rm = TRUE)
 dimen$Ancho[is.na(dimen$Ancho)] <- mean(dimen$Ancho, na.rm = TRUE)
 dimen$Alto[is.na(dimen$Alto)] <- mean(dimen$Alto, na.rm = TRUE)
 
-# cbind para unir columnas de dos data.frames
 res_limpio <- cbind(aspiradoras, dimen)
-
-# Quitamos la columna Dimensiones
 res_limpio <- res_limpio[, -7]
 
+
+# PREPROCESADO DE PRECIOS
+
+# Eliminando "US$" de los precios
+res_limpio$Precio <- gsub("US\\$", "", res_limpio$Precio)
+# Convirtiendo a numérico el campo Precio
+res_limpio$Precio <- as.numeric(res_limpio$Precio)
+
+# Como mis datos tiene productos con precios invalidos, los eliminamos
+res_limpio <- res_limpio[!is.na(res_limpio$Precio), ]
+
+# Aprobechamos y eliminamos la columna X que no nos sirve
+res_limpio <- res_limpio[, -1]
+
+# Volvemos a enumerar correctamente las filas
+n_filas <- nrow(res_limpio)
+rownames(res_limpio) <- c(1:n_filas)
+
+str(res_limpio)
 View(res_limpio)
