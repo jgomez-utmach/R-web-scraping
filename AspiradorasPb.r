dde271ef629d9731f18f1a9e6e5a52560bd2da59
jgomez-utmach <jgomez20@utmachala.edu.ec>
2024-01-07 23:10:12 -0500

    Modelo de ML en punto de API

--- AspiradorasPb.r
+++ AspiradorasPb.r
@@ -1,26 +1,28 @@
-# SIMULANDO EL CALCULO DE LA DISTANCIA ENTRE LOS CENTROIDES Y
-# LOS DATOS DE UN NUEVO ELEMENTO (campos)
+#* @param calificacion
+#* @param precio
+#* @param peso
+#* @param potencia
+#* @param largo
+#* @param ancho
+#* @param alto
+#* @get /getCluster
 
-# Nuevos datos
-campos <- as.vector(data[1, ])
-# Creo la matriz de 11x7: 11 clusters y 7 campos
-midist <- matrix(0, ncol=11, nrow=7)
+function(calificacion, precio, peso, potencia, largo, ancho, alto) {
+  # Uso los datos de los parametros para crear un vector
+  campos <- c(calificacion, precio, peso, potencia, largo, ancho, alto)
+  midist <- matrix(0, ncol=11, nrow=7)
+  for(i in 1:7){
+    c <- dist(x=c(campos[i], mycluster$centers[, i]))
+    b <- as.matrix(c)
+    distancia <- b[-1, 1]
+    midist[i,] <- distancia
+  }
+  rownames(midist) <- c("Calificacion", "Precio", "Peso", "Potencia", "Largo", "Ancho", "Alto")
 
-# Calculo la distancia entre los centroides y los datos de un nuevo elemento
-for(i in 1:7){
-  # Calculo la distancia entre los elementos de un vector
-  c <- dist(x=c(campos[i], mycluster$centers[, i]))
-  # Convierto el objeto dist en una matriz
-  b <- as.matrix(c)
-  # Elimino la primera fila y selecciono la primera columna
-  distancia <- b[-1, 1]
-  # Guardo la distancia en la matriz
-  midist[i,] <- distancia
+  # Sumo en cada columna las distancias de cada cluster
+  dist_total <- apply(midist, 2, sum)
+  # Obtengo el cluster con menor distancia
+  num_cluster <- which.min(dist_total) # cluster 3 de 11
+  # Retorno el numero de cluster
+  res <- paste("cluster_numero:", num_cluster)
 }
-# Asigno los nombres de las filas
-rownames(midist) <- c("Calificacion", "Precio", "Peso", "Potencia", "Largo", "Ancho", "Alto")
-
-# dist() calcula la distancia entre los elementos de un vector ejm:
-# dist(x=c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
-# por columna calcula la distancia entre
-# 1 y 2,3... luego entre 2 y 3,4... y así sucesivamente
