f906e818eef395516a4b7854a7651dcc652163e5
jgomez-utmach <jgomez20@utmachala.edu.ec>
2024-01-08 00:12:04 -0500

    Copiando diseño Shiny

--- AspiradorasPb.r
+++ AspiradorasPb.r
@@ -8,7 +8,6 @@
 #* @get /getCluster
 
 function(calificacion, precio, peso, potencia, largo, ancho, alto) {
-  # Uso los datos de los parametros para crear un vector
   campos <- c(calificacion, precio, peso, potencia, largo, ancho, alto)
   midist <- matrix(0, ncol=11, nrow=7)
   for(i in 1:7){
@@ -19,10 +18,7 @@ function(calificacion, precio, peso, potencia, largo, ancho, alto) {
   }
   rownames(midist) <- c("Calificacion", "Precio", "Peso", "Potencia", "Largo", "Ancho", "Alto")
 
-  # Sumo en cada columna las distancias de cada cluster
   dist_total <- apply(midist, 2, sum)
-  # Obtengo el cluster con menor distancia
-  num_cluster <- which.min(dist_total) # cluster 3 de 11
-  # Retorno el numero de cluster
+  num_cluster <- which.min(dist_total)
   res <- paste("cluster_numero:", num_cluster)
 }
--- KmeansPbMain.r
+++ KmeansPbMain.r
@@ -1,5 +1,3 @@
 library(plumber)
 r <- plumb("AspiradorasPb.r")
 r$run(port=8000)
-
-# curl "http://localhost:8000/getCluster?calificacion=0.25211944&precio=0.93263805&peso=-0.77603279&potencia=0.00000000&largo=-0.03199491&ancho=0.43090931&alto=0.61576282"
